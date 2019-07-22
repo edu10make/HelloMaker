4667424f6313d43ffe2457aea3f63e1ccc77e4b6
edu10make <47736525+edu10make@users.noreply.github.com>
2019-07-22 15:16:11 +0900

    Add files via upload

---
+++ Ch6-1_temp.ino
@@ -0,0 +1,21 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+int temp = 2;
+OneWire oneWire(temp);
+
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("온도 측정하기");
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures();
+
+  Serial.print("현재 온도: ");
+  Serial.println(sensors.getTempCByIndex(0));
+  delay(1000);
+}
---
+++ Ch6-2_servo_temp.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+int temp = 2;
+OneWire oneWire(temp);
+
+DallasTemperature sensors(&oneWire);
+
+Servo myservo;  
+int pos = 0;    
+ 
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println("온도 측정하기");
+
+  sensors.begin();
+  myservo.attach(9); 
+}
+
+void loop(void)
+{
+  sensors.requestTemperatures(); 
+  
+  Serial.print("현재 온도: ");
+  Serial.println(sensors.getTempCByIndex(0));
+  delay(1000);
+ 
+  int pos = map(sensors.getTempCByIndex(0), 15, 30, 170, 10);
+  myservo.write(pos);              
+  delay(1000);
+ 
+}
