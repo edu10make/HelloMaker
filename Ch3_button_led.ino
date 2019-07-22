5982ec51a3795d7f32765bd88e97ab5e2a0c3ab2
edu10make <47736525+edu10make@users.noreply.github.com>
2019-07-22 14:58:32 +0900

    Add files via upload

---
+++ Ch3_button_led.ino
@@ -0,0 +1,16 @@
+int LED = 3;
+int BUTTON = 7;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  if (digitalRead(BUTTON) == HIGH) {
+    digitalWrite(LED, HIGH);
+  }
+  else {
+    digitalWrite(LED, LOW);
+  }
+}
